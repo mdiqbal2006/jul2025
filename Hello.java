86b285fed883e61f0126a42fbd1b8f0a822fae87
iqbal <md.iqbal2006@gmail.com>
2025-08-07 11:46:09 +0530

    c7

--- Hello.java
+++ Hello.java
@@ -2,5 +2,7 @@ class Hello{
 public static void main(String a[]){
 System.out.println("Hello c++ guys");
 System.out.println("Hello");
+System.out.println("Hello java");
+System.out.println("Hello c++");
 }
 }
